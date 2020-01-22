5e8833c9adc12c228684eae10545f41801781cc6
Joshua Sanders <ninjosh2@gmail.com>
2020-01-22 20:57:06 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -4,6 +4,10 @@ require 'pry'
 class Scraper
 
   def self.scrape_index_page(index_url)
+    index_page = Nokogiri::HTML(open(index_url))
+    students = [] 
+    index_page.css("div.roster-cards-container").each do |card|
+      card.css(".student-card a").each do |student| 
     
   end
 
